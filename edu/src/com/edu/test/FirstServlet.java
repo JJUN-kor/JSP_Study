f24d191f8ff5f5d481ae0593ae98568c5f863261
JJUN-kor <hj0687@knu.ac.kr>
2021-01-19 19:51:37 +0900

    JSP_Study

---
+++ edu/src/com/edu/test/FirstServlet.java
@@ -0,0 +1,23 @@
+package com.edu.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class FirstServlet extends HttpServlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		//TODO Auto-generated method stub
+		System.out.println("inti()__실행");
+	}
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service()__실행");
+	}
+}
