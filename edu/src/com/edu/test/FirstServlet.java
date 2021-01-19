280bcbb9ac4883475bc44aca4e08dabf513be04d
JJUN-kor <hj0687@knu.ac.kr>
2021-01-19 19:53:18 +0900

    JSP_Study

--- edu/src/com/edu/test/FirstServlet.java
+++ edu/src/com/edu/test/FirstServlet.java
@@ -13,7 +13,7 @@ public class FirstServlet extends HttpServlet{
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		//TODO Auto-generated method stub
-		System.out.println("inti()__실행");
+		System.out.println("init()__실행");
 	}
 	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
